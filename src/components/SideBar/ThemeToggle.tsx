881bbff1089d86e8731b1784fe4d96f123ee720e
devHksingh <hksingh.dev@gmail.com>
2025-01-21 18:57:11 +0530

    add hidden prop in small screen

--- src/components/SideBar/ThemeToggle.tsx
+++ src/components/SideBar/ThemeToggle.tsx
@@ -52,8 +52,9 @@ const ThemeToggle = () => {
     <div className="  sticky top-[calc(100vh_-_48px_-_16px)] text-sm">
         
         
-        <div className="flex items-center justify-center gap-1 border rounded-md md:gap-4 border-copy-primary">
-            <span className="capitalize transition-all duration-200 text-copy-primary text-pretty">{theme}</span>
+        <div className="flex items-center justify-around gap-1 rounded-md md:gap-2 border-copy-primary">
+            <span className="hidden md:inline-block text-copy-primary text-pretty">Theme :</span>
+            <span className="hidden capitalize transition-all duration-200 text-copy-primary text-pretty md:inline-block">{theme}</span>
             <div className="flex items-center gap-2 p-1 py-2 rounded border-copy-primary">
                 <label className={` p-1 rounded-md transition-all duration-200 shadow ${theme === "light"?`border bg-cta hover:bg-cta-active`:``}`} >
                     <span><Sun className={`${theme ==="light"?`fill-orange-200 text-orange-200`:`text-stone-600`}`}/></span>
