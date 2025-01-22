d8c702692a7af2d892e5e2445463b3c37befdf20
devHksingh <hksingh.dev@gmail.com>
2025-01-22 21:16:52 +0530

    restructure ui

--- src/components/ui/LoginForm.tsx
+++ src/components/ui/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import z from 'zod'
 
 
@@ -14,6 +15,7 @@ const schema = z.object({
 })
 
 const LoginForm = () => {
+    const navigate = useNavigate();
     const {register,
         handleSubmit,
         formState:{errors}} = useForm<FormFields>({
@@ -25,13 +27,14 @@ const LoginForm = () => {
         console.log(data.email)
         console.log(data.password)
     }
+    
   return (
-    <div className="flex flex-col  justify-center w-full gap-4  rounded-lg shadow-md h-[55%] bg-dashboard">
-        <div className="flex flex-col w-full h-full gap-4 p-4 mx-auto">
-        <h1 className="self-center mt-4 text-2xl font-bold">LoginForm</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col self-center w-full p-4 mt-12 rounded shadow-md bg-card/90">
-            <label >
-                <span className="block text:md after:content-['*'] after:ml-0.5 after:text-red-500 font-semibold  mt-1 text-copy-secondary">Email</span>
+    <div className="w-full max-w-lg p-8 rounded-lg shadow-lg bg-card/75">
+        
+        <h1 className="self-center mt-1 text-2xl font-bold text-center text-copy-primary">Login</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col self-center w-full p-4 mt-2 rounded shadow-md ">
+            <label className="mt-1">
+                <span className="block text:md after:content-['*'] after:ml-0.5 after:text-red-500 font-semibold  mt-1 text-copy-secondary ">Email</span>
                 <input 
                 className="w-full p-1 mb-1 font-medium text-black border rounded peer focus:outline-none focus:border-sky-500 focus:ring-2 focus:ring-sky-500 invalid:text-red-500 invalid:focus:border-red-500 invalid:focus:ring-red-500 placeholder:text-gray-400"
                 {...register("email",{
@@ -40,8 +43,9 @@ const LoginForm = () => {
                     })} type="email" placeholder="Enter your email" />
             </label>
             {errors.email && <span className="text-sm font-medium text-red-600"> {errors.email.message}</span>}
-            <label>
-                <span className="block text:md after:content-['*'] after:ml-0.5 after:text-red-500 font-semibold  mt-1 text-copy-secondary">Password</span>
+            
+            <label className="mt-1">
+                <span className="block text:md after:content-['*'] after:ml-0.5 after:text-red-500 font-semibold  mt-1 text-copy-secondary ">Password</span>
                 <input 
                 className="w-full p-1 mb-1 font-medium text-black border rounded peer focus:outline-none focus:border-sky-500 focus:ring-2 focus:ring-sky-500 invalid:text-red-500 invalid:focus:border-red-500 invalid:focus:ring-red-500 placeholder:text-gray-400 "
                 {...register("password",
@@ -52,12 +56,31 @@ const LoginForm = () => {
                     })} type="password" placeholder="Enter the password"/>
             </label>
             {errors.password && <span className="text-sm font-medium text-red-600">{errors.password.message}</span>}
+            
             <button 
-            className={` bg-cta hover:bg-cta-active transition-colors text-cta-text font-semibold w-full py-2 rounded-md mt-3 mb-4`}
+            className={` bg-cta hover:bg-cta-active transition-colors text-cta-text font-semibold w-full py-2 rounded-md mt-6 mb-4`}
             type="submit">Submit</button>
+            <div className=" text-copy-primary">
+          <p className="text-sm ">
+            By continuing, I agree to the Terms of Use & Privacy Policy
+          </p>
+
+          <p className="">
+            Don&apos;t have an Account?{" "}
+            <span
+              className="font-semibold cursor-pointer"
+              onClick={() => navigate('/dashboard/auth/register')}
+            >
+              Create Account
+            </span>
+          </p>
+          </div>
         </form>
-        </div>
+
+        
+        
     </div>
+    
   )
 }
 
