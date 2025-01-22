dcf315761c0a5725b5db44c142feb5d34498d754
devHksingh <hksingh.dev@gmail.com>
2025-01-22 22:01:27 +0530

    redifine route

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import AuthLayout from "./layouts/AuthLayout";
+// import AuthLayout from "./layouts/AuthLayout";
+import DashboardHome from "./components/DashboardHome";
+import DashboardLayout from "./layouts/DashboardLayout";
 
 
 
 const router = createBrowserRouter([
+
     {
-        path:'/auth',
-        element:<AuthLayout/>,
+       path:'dashboard',
+       element:<DashboardLayout/>,
+       children:[
+        {
+            path:'',
+            element:<DashboardHome/>
+        },
+        {
+            path:'auth',
+        // element:<AuthLayout/>,
         children:[
             {
                 path:'login',
@@ -19,7 +30,23 @@ const router = createBrowserRouter([
                 element:<RegisterPage/>
             }
         ]
-    }
+        }
+       ]
+    },
+    // {
+    //     path:'/auth',
+    //     element:<AuthLayout/>,
+    //     children:[
+    //         {
+    //             path:'login',
+    //             element:<LoginPage/>
+    //         },
+    //         {
+    //             path:'register',
+    //             element:<RegisterPage/>
+    //         }
+    //     ]
+    // }
 ])
 
 export default router
