9d011f22bf7b28a59de845bcc92047e3bf0adfb9
devHksingh <hksingh.dev@gmail.com>
2025-01-23 18:36:48 +0530

    setup rtk query

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,17 @@ import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
 
-      <RouterProvider router={router}/>
+        <RouterProvider router={router}/>
+      </QueryClientProvider>
+      
     </Provider>
     {/* <DashboardHome /> */}
     
