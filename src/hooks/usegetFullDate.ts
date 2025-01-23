6d18238982133f92b708325898bedb2e8106ffb3
devHksingh <hksingh.dev@gmail.com>
2025-01-23 12:27:51 +0530

    add date, time, text

--- src/hooks/usegetFullDate.ts
+++ src/hooks/usegetFullDate.ts
@@ -3,18 +3,33 @@ import { useEffect, useState } from "react"
 const useGetFullDate = () => {
   const [date, setDate] = useState("")
   const [time, setTime] = useState("")
-
+  
+  const [text,setText]= useState("")
   function getTimeAndDate() {
-    const currentDate = new Date()  
+    const currentDate = new Date()
     const arr = String(currentDate).split(" ")
     setDate(`${arr[0]}, ${arr[1]} ${arr[2]} ${arr[3]}`)
-    setTime(`${arr[4]}`)
+    setTime(`${String(currentDate.toLocaleString('en-US')).split(',')[1]}`)
+    const hrs = Number(arr[4].split(':')[0])
+    if (hrs >= 0 && hrs < 12) {
+      setText("morning")
+    } else if (hrs === 12) {
+      setText("noon")
+    } else if (hrs > 12 && hrs < 17) {
+      setText("afternoon")
+    } else if (hrs >= 17 && hrs < 21) {  
+      setText("evening")
+    } else {
+      setText("night")
+    }
+    
+    
   }
 
   useEffect(() => {
-    
+
     getTimeAndDate()
-    
+
     const timer = setInterval(() => {
       getTimeAndDate()
     }, 1000)
@@ -23,8 +38,8 @@ const useGetFullDate = () => {
       clearInterval(timer)
     }
   }, [])
-  
-  return { fullDate: date, time }  
+
+  return { fullDate: date, time, text }
 }
 
 export default useGetFullDate
