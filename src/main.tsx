916b6d3f6af7f8a7fbcc7255701e23e3af2807cf
devHksingh <hksingh.dev@gmail.com>
2025-01-26 12:25:09 +0530

    add auth reducer

--- src/main.tsx
+++ src/main.tsx
@@ -10,13 +10,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <QueryClientProvider client={queryClient}>
 
         <RouterProvider router={router}/>
       </QueryClientProvider>
       
-    {/* </Provider> */}
+    </Provider>
     
     
   </StrictMode>,
