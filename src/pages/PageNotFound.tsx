f85504eb61a5f557f75d99836f74bbedeb758309
devHksingh <hksingh.dev@gmail.com>
2025-02-06 20:15:20 +0530

    minor change

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -8,7 +8,7 @@ const PageNotFound = () => {
         <p className="text-6xl font-bold tracking-wider text-gray-300 md:text-7xl lg:text-9xl">404</p>
         <p className="mt-4 text-2xl font-bold tracking-wider text-gray-500 md:text-3xl lg:text-5xl">Page Not Found</p>
         <p className="pb-4 mt-4 text-center text-gray-500 border-b-2">Sorry, the page you are looking for could not be found.</p>
-        <Link to={'/dashboard'} className="flex items-center px-4 py-2 mt-6 space-x-2 text-gray-100 transition duration-150 bg-blue-600 rounded hover:bg-blue-700" title="Return Home">
+        <Link to={'/dashboard'} className="flex items-center px-4 py-2 mt-6 space-x-2 text-gray-100 transition duration-150 bg-blue-600 rounded hover:bg-blue-700" >
             <MoveLeftIcon/>
             <span>Return Home</span>
         </Link>
