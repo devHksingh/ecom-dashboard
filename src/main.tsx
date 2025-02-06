805ea5f4625d233e884983241ad1cbf3c5e1a6ac
devHksingh <hksingh.dev@gmail.com>
2025-02-06 20:19:17 +0530

    add stale and cache time in react query

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,16 @@ import { Provider } from 'react-redux'
 import { store } from './app/store.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions:{
+    // TODO: Change stale and cache time in production
+    queries:{
+      staleTime: 40*(60*1000), // 40 mins
+      gcTime:15*(60*1000), // 15 mins cacheTime
+      refetchIntervalInBackground:true 
+    }
+  }
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
