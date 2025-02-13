c0eeb890395d2ccc57b651153c35a1bc8ded22f6
devHksingh <hksingh.dev@gmail.com>
2025-02-13 11:47:15 +0530

    add table loading state

---
+++ src/components/skeleton/TableLoader.tsx
@@ -0,0 +1,104 @@
+
+const TableLoader = () => {
+  return (
+    <div className="pt-14 animate-pulse">
+        {/* <div className="flex">
+            <div className="h-8 mt-3 mb-6 bg-stone-600 rounded-s-lg w-[80%]"></div>
+            <div className="h-8 mt-3 mb-6 bg-gray-400 rounded-e-lg w-[20%]"></div>
+        </div> */}
+        <div className="border rounded ">
+        <div className="flex divide-x-2">
+            <div className="h-8  mb-6 bg-stone-600  w-[16.6%]"></div>
+            <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+            <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+            <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+            <div className="h-8  mb-6 bg-stone-600  w-[16.76%]"></div>
+            <div className="h-8   bg-gray-400  w-[16.66%]"></div>
+            
+        </div>
+        <div className="">
+            <div className="flex w-full divide-x-2">
+                <div className="h-8  mb-6 bg-stone-600  w-[16.6%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.76%]"></div>
+                <div className="h-8   bg-gray-400  w-[16.66%]"></div>
+            </div>
+            <div className="flex w-full divide-x-2">
+                <div className="h-8  mb-6 bg-stone-600  w-[16.6%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8   bg-gray-400  w-[16.76%]"></div>
+            </div>
+            <div className="flex w-full divide-x-2">
+                <div className="h-8  mb-6 bg-stone-600  w-[16.6%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8   bg-gray-400  w-[16.76%]"></div>
+            </div>
+            <div className="flex w-full divide-x-2">
+                <div className="h-8  mb-6 bg-stone-600  w-[16.6%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8   bg-gray-400  w-[16.76%]"></div>
+            </div>
+            <div className="flex w-full divide-x-2">
+                <div className="h-8  mb-6 bg-stone-600  w-[16.6%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8   bg-gray-400  w-[16.76%]"></div>
+            </div>
+            <div className="flex w-full divide-x-2">
+                <div className="h-8  mb-6 bg-stone-600  w-[16.6%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8   bg-gray-400  w-[16.76%]"></div>
+            </div>
+            <div className="flex w-full divide-x-2">
+                <div className="h-8  mb-6 bg-stone-600  w-[16.6%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8   bg-gray-400  w-[16.76%]"></div>
+            </div>
+            <div className="flex w-full divide-x-2">
+                <div className="h-8  mb-6 bg-stone-600  w-[16.6%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-gray-400  w-[16.66%]"></div>
+                <div className="h-8  mb-6 bg-stone-600  w-[16.66%]"></div>
+                <div className="h-8   bg-gray-400  w-[16.76%]"></div>
+            </div>
+        </div>
+        
+            
+                {/* <div className="h-6 mb-6 rounded bg-stone-600 w-[100%]"></div>
+                <div className="h-6 mb-6 bg-gray-400 rounded w-[100%]"></div>
+                <div className="h-6 mb-6 rounded bg-stone-600 w-[100%]"></div>
+                <div className="h-6 mb-6 bg-gray-400 rounded w-[100%]"></div>
+                <div className="h-6 mb-6 rounded bg-stone-600 w-[100%]"></div>
+                <div className="h-6 mb-6 bg-gray-400 rounded w-[100%]"></div>
+                <div className="h-6 mb-6 rounded bg-stone-600 w-[100%]"></div>
+                <div className="h-6 mb-6 bg-gray-400 rounded w-[100%]"></div>
+                <div className="h-6 mb-6 rounded bg-stone-600 w-[100%]"></div>
+                <div className="h-6 mb-6 bg-gray-400 rounded w-[100%]"></div> */}
+        </div>
+        
+        
+    </div>
+  )
+}
+
+export default TableLoader
