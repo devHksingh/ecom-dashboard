2f967233e005e405828e7d1d0a6600a04eb7bfd2
devHksingh <hksingh.dev@gmail.com>
2025-03-01 19:02:10 +0530

    add navigation in useeffect to fix hook side effect

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,42 +3,43 @@ import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../app/store"
 import { addUserDetails } from "../features/auth/authSlice"
 import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
 
 const useAuth = () => {
     const navigate = useNavigate()
     const userData = useSelector((state: RootState) => state.auth)
     const dispatch = useDispatch()
-    if (!userData.accessToken ||!userData.isLogin) {
-        const userSessionData = JSON.parse(sessionStorage.getItem('user') || `{}`)
-        if (userSessionData.accessToken) {
-            const { accessToken, refreshToken, id, name, email } = userSessionData
-            dispatch(addUserDetails({ accessToken, refreshToken, userId:id, userName:name, useremail:email,isLogin:true }))
-            navigate('/',{replace:true})
+    // if (!userData.accessToken ||!userData.isLogin) {
+    //     const userSessionData = JSON.parse(sessionStorage.getItem('user') || `{}`)
+    //     if (userSessionData.accessToken) {
+    //         const { accessToken, refreshToken, id, name, email } = userSessionData
+    //         dispatch(addUserDetails({ accessToken, refreshToken, userId:id, userName:name, useremail:email,isLogin:true }))
+    //         navigate('/',{replace:true})
             
-        }else{
-            navigate('/dashboard/auth/login',{replace:true})
-            
-        }
-    }
-    // useEffect(() => {
-    //     const checkAuth =  () => {
-    //         if (!userData.accessToken ||!userData.isLogin) {
-    //             const userSessionData = JSON.parse(sessionStorage.getItem('user') || `{}`)
-    //             if (userSessionData.accessToken) {
-    //                 const { accessToken, refreshToken, id, name, email } = userSessionData
-    //                 dispatch(addUserDetails({ accessToken, refreshToken, userId:id, userName:name, useremail:email,isLogin:true }))
-                    
-                    
-    //             }else{
-    //                 navigate('/dashboard/auth/login',{replace:true})
-                    
-    //             }
-    //         }
-            
+    //     }else{
+    //         navigate('/dashboard/auth/login',{replace:true})
             
     //     }
-    //     checkAuth()
-    // }, [])
+    // }
+    useEffect(() => {
+        const checkAuth =  () => {
+            if (!userData.accessToken ||!userData.isLogin) {
+                const userSessionData = JSON.parse(sessionStorage.getItem('user') || `{}`)
+                if (userSessionData.accessToken) {
+                    const { accessToken, refreshToken, id, name, email } = userSessionData
+                    dispatch(addUserDetails({ accessToken, refreshToken, userId:id, userName:name, useremail:email,isLogin:true }))
+                    navigate('/',{replace:true})
+                    
+                }else{
+                    navigate('/dashboard/auth/login',{replace:true})
+                    
+                }
+            }
+            
+            
+        }
+        checkAuth()
+    }, [])
 
     
 }
