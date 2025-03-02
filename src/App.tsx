9e12729e45c73980a541a9ecc4c62a84df6c1668
devHksingh <hksingh.dev@gmail.com>
2025-03-02 21:12:06 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import { addUserDetails } from './features/auth/authSlice';
 
 
+
 function App() {
   
   const [loading,setLoading] = useState(true)
