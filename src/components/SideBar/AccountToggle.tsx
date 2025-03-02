6535fc7822ec32ee74a3a9d3da9a8968b58af4b9
devHksingh <hksingh.dev@gmail.com>
2025-03-02 21:11:54 +0530

    add user details if login

--- src/components/SideBar/AccountToggle.tsx
+++ src/components/SideBar/AccountToggle.tsx
@@ -1,6 +1,21 @@
 import { ChevronsUpDown } from "lucide-react"
+import { useSelector } from "react-redux"
+import { RootState } from "../../app/store"
+import { useEffect, useState } from "react"
 
 const AccountToggle = () => {
+  const [isLoginUser,setIsLoginUser] = useState(false)
+  const [userName,setUserName] = useState("")
+  const [userEmail,setuserEmail] = useState("")
+  const userData = useSelector((state:RootState)=>state.auth)
+  useEffect(()=>{
+    if(userData.isLogin){
+      setIsLoginUser(true)
+      setUserName(userData.userName)
+      setuserEmail(userData.useremail)
+    }
+  },[])
+  
   return (
     <div className="pb-4 mt-2 mb-4 border-b border-stone-300">
          <button className="flex p-0.5 hover:bg-copy-primary/10 rounded transition-colors  gap-2 w-full items-center justify-between">
@@ -10,8 +25,8 @@ const AccountToggle = () => {
           className="rounded shadow size-8 shrink-0 bg-violet-500"
         />
         <div className="text-start ml-[-14%]">
-          <span className="block text-sm font-bold text-copy-primary">UserName</span>
-          <span className="block text-xs font-medium text-copy-secondary">Email Id</span>
+          <span className="block text-sm font-bold text-copy-primary">{isLoginUser?`${userName}`:"UserName"}</span>
+          <span className="block text-xs font-medium text-copy-secondary"> {isLoginUser?`${userEmail}`:"Email Id"}</span>
         </div>
 
         <ChevronsUpDown strokeWidth={1} className="ml-5 text-copy-primary" />
