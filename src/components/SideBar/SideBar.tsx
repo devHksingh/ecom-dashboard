5da72eb1ef3724c88757a76e7481b9991e1b75ab
devHksingh <hksingh.dev@gmail.com>
2025-03-02 21:12:25 +0530

    add routeselect comp

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,4 +1,5 @@
 import AccountToggle from "./AccountToggle"
+import RouteSelect from "./RouteSelect"
 import ThemeToggle from "./ThemeToggle"
 
 
@@ -8,6 +9,7 @@ const SideBar = () => {
         <div className="sticky overflow-y-hidden top-4 h-[calc(100vh-32px-48px)]">
             {/* Main side bar content */}
             <AccountToggle/>
+            <RouteSelect/>
         </div>
         {/* Theme toggle */}
         <ThemeToggle/>
