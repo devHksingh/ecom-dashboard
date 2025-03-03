52d52abe68b166e9712069c4f4335a048e98dd93
devHksingh <hksingh.dev@gmail.com>
2025-03-03 19:13:21 +0530

    hide text

--- src/components/SideBar/ThemeToggle.tsx
+++ src/components/SideBar/ThemeToggle.tsx
@@ -55,7 +55,7 @@ const ThemeToggle = () => {
         <div className="flex items-center justify-around gap-1 rounded-md md:gap-2 border-copy-primary">
             <span className="hidden md:inline-block text-copy-primary text-pretty">Theme :</span>
             <span className="hidden capitalize transition-all duration-200 text-copy-primary text-pretty md:inline-block">{theme}</span>
-            <div className="flex items-center gap-2 p-1 py-2 rounded border-copy-primary">
+            <div className="flex flex-col items-center gap-2 p-1 py-2 rounded border-copy-primary md:flex-row">
                 <label className={` p-1 rounded-md transition-all duration-200 shadow ${theme === "light"?`border bg-cta hover:bg-cta-active`:``}`} >
                     <span><Sun className={`${theme ==="light"?`fill-orange-200 text-orange-200`:`text-stone-600`}`}/></span>
                     <input type="checkbox"  aria-label="light-theme-button" id="" checked={isChecked} onChange={handleCheckboxChange} className="sr-only"/>
