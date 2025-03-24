b55d12944bf3fffe2f5735d6df03a3ab4dde994e
devHksingh <hksingh.dev@gmail.com>
2025-03-24 17:42:56 +0530

    add routeradd link tag

--- src/components/SideBar/AccountToggle.tsx
+++ src/components/SideBar/AccountToggle.tsx
@@ -2,6 +2,7 @@ import { ChevronsUpDown } from "lucide-react"
 import { useSelector } from "react-redux"
 import { RootState } from "../../app/store"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 const AccountToggle = () => {
   const [isLoginUser,setIsLoginUser] = useState(false)
@@ -14,11 +15,11 @@ const AccountToggle = () => {
       setUserName(userData.userName)
       setuserEmail(userData.useremail)
     }
-  },[])
+  },[userData.isLogin, userData.userName, userData.useremail])
   
   return (
     <div className="pb-4 mt-2 mb-4 border-b border-stone-300">
-         <button className="flex p-0.5 hover:bg-copy-primary/10 rounded transition-colors  gap-2 w-full items-center justify-between">
+         <Link to={'/dashboard/userProfile/'} className="flex p-0.5 hover:bg-copy-primary/10 rounded transition-colors  gap-2 w-full items-center justify-between">
         <img
           src="https://api.dicebear.com/9.x/notionists/svg"
           alt="avatar"
@@ -30,7 +31,7 @@ const AccountToggle = () => {
         </div>
 
         <ChevronsUpDown strokeWidth={1} className="ml-5 text-copy-primary" />
-      </button>
+      </Link>
     </div>
   )
 }
